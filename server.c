7d630a35753c95b3802029b3570d20408d38b8ac
NadVev <nadav.levi@campus.technion.ac.il>
2024-03-06 21:02:59 +0000

    added part2

--- server.c
+++ server.c
@@ -40,6 +40,33 @@
         return;
     }
 
+    void remove_by_index (struct Queue* q, int index){         //great potential of bugs
+        if (q->c_size>index && index>0){
+            for (int j = index; j!=q->c_first; j= (j-1+q->c_queue_max_size)%q->c_queue_max_size){
+                q->c_queue[j] = q->c_queue[(j-1+q->c_queue_max_size)%q->c_queue_max_size];
+            }
+            q->c_first = (q->c_first+1)%q->c_queue_max_size;
+            q->c_size--;
+        }
+    }
+
+    /*
+        void remove_value (struct Queue* q, int value){         //great potential of bugs
+            if (q->c_size>0){
+                for (int i = q->c_first; i!=q->c_last; i= (i+1)%q->c_queue_max_size){
+                    if (q->c_queue[i]==value){
+                        for (int j = i; j!=q->c_first; j= (j-1+q->c_queue_max_size)%q->c_queue_max_size){
+                            q->c_queue[j] = q->c_queue[(j-1+q->c_queue_max_size)%q->c_queue_max_size];
+                        }
+                        q->c_first = (q->c_first+1)%q->c_queue_max_size;
+                        q->c_size--;
+                        break;
+                    }
+                }
+            }
+        }
+    */
+
     void pop (struct Queue* q){
         if (q->c_size>0){
             q->c_first = (q->c_first+1)%q->c_queue_max_size;
@@ -65,11 +92,12 @@
 struct thread_args{
     struct Queue* waiting_tasks;
     struct Queue* running_tasks;
+    char* schedalg;
 };
 
 
 
-pthread_cond_t c1, c2; // should be initialized
+pthread_cond_t c1, c2, c3; // should be initialized
 pthread_mutex_t m; // should be initialized
 
 
@@ -92,6 +120,7 @@ int main(int argc, char *argv[])
 {
     pthread_cond_init(&c1, NULL);
     pthread_cond_init(&c2, NULL);
+    pthread_cond_init(&c3, NULL);
     pthread_mutex_init(&m, NULL);
 
     int listenfd, connfd, port, clientlen;
@@ -102,7 +131,7 @@ int main(int argc, char *argv[])
     struct sockaddr_in clientaddr;
 
     getargs(&port, &threads_num, &queue_max_size, &schedalg, argc, argv);
-    if (queue_max_size==0 || threads_num==0){
+    if (queue_max_size==0 || threads_num==0){       //check invalid schedalg
         printf ("invalid input\n");          //check format
     }
 
@@ -113,6 +142,7 @@ int main(int argc, char *argv[])
     struct thread_args* args = (struct thread_args*)malloc(sizeof(struct thread_args));
     args->waiting_tasks = waiting_tasks;
     args->running_tasks = running_tasks;
+    args->schedalg = schedalg;
 
 
     pthread_t* threads = (pthread_t*)malloc(sizeof(pthread_t)*threads_num);
@@ -127,16 +157,42 @@ int main(int argc, char *argv[])
 
     listenfd = Open_listenfd(port);
     while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
 
-	mutex_lock(&m);
-    while (waiting_tasks->c_size+running_tasks->c_size >= queue_max_size){
-        cond_wait (&c2, &m);        //add wrap function?
-    }
-    add (waiting_tasks, connfd);
-    cond_signal (&c1);          //add wrap function?
-    mutex_unlock (&m);          //add wrap function?
+        mutex_lock(&m);
+        if (waiting_tasks->c_size+running_tasks->c_size >= queue_max_size){
+            if (strcasecmp(schedalg, "block")){
+                while (waiting_tasks->c_size+running_tasks->c_size >= queue_max_size){
+                    cond_wait (&c2, &m);        //add wrap function?
+                }
+            }
+            else if (strcasecmp(schedalg, "dt")){
+                Close(connfd);
+                continue;
+            }
+            else if (strcasecmp(schedalg, "dh")){
+                int fd_for_remove = top (waiting_tasks);
+                Close(fd_for_remove);
+                pop (waiting_tasks);
+            }
+            else if (strcasecmp(schedalg, "bf")){
+                while (waiting_tasks->c_size+running_tasks->c_size > 0){
+                    cond_wait (&c3, &m);        //add wrap function?
+                }
+            }
+            else if (strcasecmp(schedalg, "random")){
+                drop_random(waiting_tasks);
+                if (waiting_tasks->c_size+running_tasks->c_size >= queue_max_size){     //check what to do in this case
+                    Close(connfd);
+                    continue;   
+                }
+            }
+        }
+
+        add (waiting_tasks, connfd);
+        cond_signal (&c1);          //add wrap function?
+        mutex_unlock (&m);          //add wrap function?
     }
 
 //we will arrive here ??
@@ -169,9 +225,26 @@ void thread_routine(struct thread_args* args){
 	    Close(curr_task);
 
         mutex_lock (&m);                    //add wrap function?
-        pop (args->running_tasks);
-        cond_signal (&c2);                    //add wrap function?
+        pop (args->running_tasks);                  //should use remove_value(args->running_tasks, curr_task)
+        if (strcasecmp(args->schedalg, "block")){
+            cond_signal (&c2);                    //add wrap function?
+        }
+        else if (strcasecmp(args->schedalg, "bf") && (args->waiting_tasks->c_size+args->running_tasks->c_size == 0)){
+            cond_signal (&c3);
+        }
         mutex_unlock (&m);                 //add wrap function?
     }
 }
- 
+
+void drop_random (struct Queue* waiting_tasks){
+    int drop_count = (waiting_tasks->c_size+1)/2;
+    for(int i = 0; i<drop_count; i++) {
+        if(waiting_tasks->c_size == 0){
+            break;
+        }    
+        int random_index = rand() % waiting_tasks->c_size;
+        Close(waiting_tasks->c_queue[random_index]);        //not good
+        remove_by_index(waiting_tasks, random_index);
+        cond_signal (&c2);          //think about it
+    }
+}
